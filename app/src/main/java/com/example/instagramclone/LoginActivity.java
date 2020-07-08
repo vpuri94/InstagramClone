dda8b68a186f4a79db90f2e06dbdd636bed8d965
Viraj Puri <vp1@fb.com>
2020-07-08 10:15:07 -0400

    Sign up feature added

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +22,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,8 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,6 +48,41 @@ public class LoginActivity extends AppCompatActivity {
                 loginUser(username, password);
             }
         });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick signup button");
+                String newName = etUsername.getText().toString();
+                String newPword = etPassword.getText().toString();
+                signUp(newName, newPword);
+            }
+        });
+    }
+
+    private void signUp(String newName, String newPword) {
+        Log.i(TAG, "Attempting to signup user " + newName);
+        // Create new ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(newName);
+        user.setPassword(newPword);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+                else{
+                    // Sign up worked
+                    goMainActivity();
+                    Toast.makeText(LoginActivity.this, "Signed up successfully!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     private void loginUser(String username, String password) {
